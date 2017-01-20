189fa8720a7dfaab0f2bf5819c72cba14ec960cf
Eirik F. Wahl <eirik.wahl@itema.no>
2017-01-21 00:00:14 +0100

    Make Waves into prefabs (still test, but getting close)

--- JumpWaveGame/Assets/Scripts/WaveController.cs
+++ JumpWaveGame/Assets/Scripts/WaveController.cs
@@ -1,44 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
-using System.ComponentModel;
 
 public class WaveController : MonoBehaviour {
-    public Transform[] cylinders; 
+    public Transform CylinderPrefab; 
+    public Material WaveMaterial;
 
-    public float RadiusToLengthFactor;
+    public int NumberOfCylinders = 16;
+    public float RadiusToLengthFactor = 0.2f;
+    public float RadiusExpansionRate = 3;
 
-    public float CurrentWaveRadius;
+    private Transform[] cylinders; 
 
-    public float RadiusExpansionRate = 1;
+    [SerializeField]
+    private float CurrentWaveRadius;
 
-    // Private variables:
-    public float angleDelta;
+    [SerializeField]
+    private float angleDelta;
 
 	public void Start () {
-        Assert.IsNotNull(cylinders, "cylinders must be initialized");
-        Assert.AreEqual(16, cylinders.Length);
+        Assert.IsNotNull(CylinderPrefab, "CylinderTemplate must be initialized");
+        cylinders = new Transform[NumberOfCylinders];
 
-        angleDelta = 360.0f / cylinders.Length;
+        angleDelta = 360.0f / NumberOfCylinders;
 
         // Reset all positions:
-        for (int i = 0; i < cylinders.Length; i++) {
-            Transform cylinder = cylinders[i];
+        for (int i = 0; i < NumberOfCylinders; i++) {
+            Transform cylinder = Object.Instantiate(CylinderPrefab);
+            cylinder.gameObject.GetComponent<MeshRenderer>().sharedMaterial = WaveMaterial;
+            cylinder.parent = this.transform;
             cylinder.localPosition = Vector3.zero;
             cylinder.localEulerAngles = new Vector3(90, -i * angleDelta, 0);
             cylinder.localScale = new Vector3(1, 0, 1);
+            cylinders[i] = cylinder;
         }
 	}
 	
-    public void Update () {
-
-    }
-
     public void FixedUpdate () {
         CurrentWaveRadius += RadiusExpansionRate * Time.fixedDeltaTime;
 
-        for (int i = 0; i < cylinders.Length; i++) {
+        for (int i = 0; i < NumberOfCylinders; i++) {
             Transform cylinder = cylinders[i];
             cylinder.localPosition = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleDelta * i) * CurrentWaveRadius, 0, Mathf.Sin(Mathf.Deg2Rad * angleDelta * i) * CurrentWaveRadius);
             cylinder.localScale = new Vector3(1, CurrentWaveRadius * RadiusToLengthFactor + Mathf.Sin(Mathf.Deg2Rad * angleDelta / 2.0f) * 0.5f, 1);
