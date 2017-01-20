35489a4b8e093b493d38305b975a9c540eecd4b5
Eirik F. Wahl <eirik.wahl@itema.no>
2017-01-21 00:06:19 +0100

    Make wave width (cylinder diameter) settable in editor.

--- JumpWaveGame/Assets/Scripts/WaveController.cs
+++ JumpWaveGame/Assets/Scripts/WaveController.cs
@@ -5,6 +5,7 @@ public class WaveController : MonoBehaviour {
     public Transform CylinderPrefab; 
     public Material WaveMaterial;
 
+    public float CylinderDiameter = 1;
     public int NumberOfCylinders = 16;
     public float RadiusToLengthFactor = 0.2f;
     public float RadiusExpansionRate = 3;
@@ -30,7 +31,7 @@ public class WaveController : MonoBehaviour {
             cylinder.parent = this.transform;
             cylinder.localPosition = Vector3.zero;
             cylinder.localEulerAngles = new Vector3(90, -i * angleDelta, 0);
-            cylinder.localScale = new Vector3(1, 0, 1);
+            cylinder.localScale = new Vector3(CylinderDiameter, 0, CylinderDiameter);
             cylinders[i] = cylinder;
         }
 	}
@@ -41,7 +42,7 @@ public class WaveController : MonoBehaviour {
         for (int i = 0; i < NumberOfCylinders; i++) {
             Transform cylinder = cylinders[i];
             cylinder.localPosition = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleDelta * i) * CurrentWaveRadius, 0, Mathf.Sin(Mathf.Deg2Rad * angleDelta * i) * CurrentWaveRadius);
-            cylinder.localScale = new Vector3(1, CurrentWaveRadius * RadiusToLengthFactor + Mathf.Sin(Mathf.Deg2Rad * angleDelta / 2.0f) * 0.5f, 1);
+            cylinder.localScale = new Vector3(CylinderDiameter, CurrentWaveRadius * RadiusToLengthFactor + Mathf.Sin(Mathf.Deg2Rad * angleDelta / 2.0f) * (CylinderDiameter/2.0f), CylinderDiameter);
         }
 
     }
