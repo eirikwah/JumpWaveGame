ead2445e672f0856d9b5f77caaa0223f25a7b7fb
Eirik F. Wahl <eirik.wahl@itema.no>
2017-01-22 00:02:55 +0100

    Sounds for player hit by kick and wave

---
+++ JumpWaveGame/Assets/Scripts/PlayerHitSoundPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class PlayerHitSoundPlayer : MonoBehaviour {
+    [FMODUnity.EventRef]
+    public string KickHitSound = "event:/KickHit";
+
+    [FMODUnity.EventRef]
+    public string WaveHitSound = "event:/Hit";
+
+    public void OnCollisionEnter(Collision collision) {
+        //Debug.Log("Hit collider " + collision.gameObject.name);
+        if (collision.gameObject.name.Equals("AttackCollider"))
+        {
+            RuntimeManager.PlayOneShot(KickHitSound, Vector3.zero);
+        }
+        else if (collision.gameObject.name.StartsWith("Wave"))
+        {
+            RuntimeManager.PlayOneShot(WaveHitSound, Vector3.zero);
+        }
+        else
+        {
+            Debug.Log("No sound for hitting collider " + collision.gameObject.name);
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.name.Equals("AttackCollider"))
+        {
+            RuntimeManager.PlayOneShot(KickHitSound, Vector3.zero);
+        }
+        else if (collider.gameObject.name.StartsWith("Wave"))
+        {
+            RuntimeManager.PlayOneShot(WaveHitSound, Vector3.zero);
+        }
+        else
+        {
+            Debug.Log("No sound for hitting trigger " + collider.gameObject.name);
+        }
+    }
+
+}
