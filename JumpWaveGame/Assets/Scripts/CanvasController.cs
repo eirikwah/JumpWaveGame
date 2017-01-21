58180377496458a9f6ec4d1021c0b13970c63ae4
Emil Grunt <emilgrunt@gmail.com>
2017-01-21 16:09:40 +0100

    Fixes an animation bug in CanvasController

--- JumpWaveGame/Assets/Scripts/CanvasController.cs
+++ JumpWaveGame/Assets/Scripts/CanvasController.cs
@@ -7,13 +7,36 @@ using DG.Tweening;
 public class CanvasController : MonoBehaviour {
 	public float FullCountdownDuration;
 
+	private Sequence animationSequence;
+
 	void Start() {
 		Debug.Assert(transform.childCount > 0, "Missing CountdownItems for the Canvas");
 		Debug.Assert(FullCountdownDuration > 0.0f, "The Countdown duration must be larger than zero.");
 
+		float duration = FullCountdownDuration / transform.childCount;
+
+		// Create the animation sequence and an initial pause.
+		animationSequence = DOTween.Sequence();
+		animationSequence.AppendInterval(0.5f);
+
 		foreach (Transform transform in transform) {
 			CanvasRenderer renderer = transform.GetComponent<CanvasRenderer>();
 
+			animationSequence.Append(DOTween.ToAlpha(renderer.GetColor, renderer.SetColor, 1.0f, 0.1f));
+			animationSequence.Append(transform.DOScale(new Vector3(2, 2, 1), duration - 0.1f));
+			animationSequence.Append(DOTween.ToAlpha(renderer.GetColor, renderer.SetColor, 0.0f, duration));
+		}
+
+
+		animationSequence.OnComplete(() => {
+			animationSequence.Rewind();
+			gameObject.SetActive(false);
+		});
+	}
+
+	void SetChildrenAsInvisible() {
+		foreach (Transform transform in transform) {
+			CanvasRenderer renderer = transform.GetComponent<CanvasRenderer>();
 			renderer.SetAlpha(0.0f);
 		}
 	}
@@ -23,20 +46,8 @@ public class CanvasController : MonoBehaviour {
 	}
 
 	private void StartCountdown() {
-		float duration = FullCountdownDuration / transform.childCount;
+		SetChildrenAsInvisible();
 
-		Sequence sequence = DOTween.Sequence();
-		sequence.AppendInterval(0.5f);
-
-		int transformIndex = 0;
-		foreach (Transform transform in transform) {
-			var renderer = transform.GetComponent<CanvasRenderer>();
-
-			sequence.Append(DOTween.ToAlpha(renderer.GetColor, renderer.SetColor, 1.0f, 0.2f));
-			sequence.Append(transform.DOScale(new Vector3(2, 2, 1), duration));
-			sequence.PrependInterval(transformIndex * duration - 0.2f);
-			sequence.Append(DOTween.ToAlpha(renderer.GetColor, renderer.SetColor, 0.0f, duration));
-			sequence.OnComplete(() => gameObject.SetActive(false));
-		}
+		animationSequence.Restart();
 	}
 }
