ebfb42ea92ac9c874f670100ba8cd657199f3ce6
Eirik F. Wahl <eirik.wahl@itema.no>
2017-01-21 21:55:03 +0100

    Test wave sound player

---
+++ JumpWaveGame/Assets/Scripts/WaveSoundPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSoundPlayer : MonoBehaviour {
+    [FMODUnity.EventRef]
+    public string WaveSound = "event:/WaveImpact";
+
+	// Use this for initialization
+	public void Start () {
+        FMODUnity.RuntimeManager.PlayOneShot(WaveSound, Vector3.zero);
+	}
+	
+}
