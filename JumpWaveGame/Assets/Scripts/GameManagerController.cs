fc39766da7a1e9cd43312abdb0ffe553df31b5aa
Emil Grunt <emilgrunt@gmail.com>
2017-01-21 16:45:55 +0100

    Adds initial GameManager script

---
+++ JumpWaveGame/Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class GameManagerController : MonoBehaviour {
+	public Canvas CountdownCanvas;
+
+	public Canvas StartScreenCanvas;
+
+	public float fadeOutDuration;
+
+	void Start () {
+		Debug.Assert(CountdownCanvas, "GameManager needs a reference to the CountdownCanvas");
+		Debug.Assert(StartScreenCanvas, "GameManager needs a reference to the StartScreenCanvas");
+
+		StartScreenCanvas.gameObject.SetActive(true);
+		var renderer = StartScreenCanvas.GetComponent<CanvasRenderer>();
+
+		DOTween.ToAlpha(renderer.GetColor, renderer.SetColor, 0.0f, 1.0f);
+	}
+
+	void Update () {
+		
+	}
+
+	void OnEnable() {
+	}
+}
