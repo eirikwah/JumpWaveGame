9184647622a2ff177413218d5fe888404a148601
Eirik F. Wahl <eirik.wahl@itema.no>
2017-01-22 11:12:54 +0100

    DeathDetector: Detect when players exit the playable area
    Makes them scream + Diables the player (effect will be added later?)

---
+++ JumpWaveGame/Assets/Scripts/DeathDetector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+public class DeathDetector : MonoBehaviour {
+    [FMODUnity.EventRef]
+    public string VictorySound = "event:/Victory";
+
+    [FMODUnity.EventRef]
+    public string DeathByFallingSound = "event:/WilhelmScream";
+
+    public void OnCollisionExit(Collision collision) {
+        var player = collision.gameObject;
+        CheckGameobjectLeavingLifeCube(player, "collision exit");
+    }
+
+    public void OnTriggerExit(Collider collider) {
+        var player = collider.gameObject;
+        CheckGameobjectLeavingLifeCube(player, "trigger exit");
+    }
+
+    private void CheckGameobjectLeavingLifeCube(GameObject player, string debugText) {
+        Debug.Log("Went outside the life cube by " + debugText + "; " + player.name);
+        if (player.name.StartsWith("Player"))
+        {
+            // If this really is a player:
+            RuntimeManager.PlayOneShot(DeathByFallingSound, Vector3.zero);
+
+            player.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Ignoring non-player falling outside the life cube: " + player.name);
+        }
+
+    }
+}
